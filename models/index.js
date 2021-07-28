f81240cd2ce6d471e68f511f65de98e88b0d0c00
kdchurch1994 <kdchurch@bsu.edu>
2021-07-28 19:02:59 -0400

    Lesson 4 Complete!

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const User = require("./User");
 const Post = require("./Post")
+const Vote = require("./Vote");
 
-module.exports = { User, Post };
+
 
 //Create associations between the models
 User.hasMany(Post, {            //User can make many posts. hasMany is used to establish a one to many relationship
@@ -11,3 +12,33 @@ User.hasMany(Post, {            //User can make many posts. hasMany is used to e
 Post.belongsTo(User, {          //A post can belong to one user, but not many users (A many to one relationship)
     foreignKey: 'user_id',
 });
+
+User.belongsToMany(Post, {      //With these two .belongsToMany() methods in place, we're allowing both the User and Post models to query each other's information in the context of a vote.
+    through: Vote,              //If we want to see which users voted on a single post, we can now do that. If we want to see which posts a single user voted on, we can see that too. 
+    as: 'voted_posts',          //This makes the data more robust and gives us more capabilites for visualizing this data on the client-side.
+    foreignKey: 'user_id'       //Notice the syntax. We instruc the application that the User and Post models will be connected, but in this case through the Vote model.
+});                             //We state what we want the foreign key to be in Vote, which aligns with the fields we set up in the model.
+                                //We also stipulate that the name of the Vote model should be displayed as voted_posts, when queired on, making it a litte more informative. 
+Post.belongsToMany(User, {      //Frthermore, the Vote table needs a row of data to be a unique pairing so that it knows which data to pull in when queried on.
+    through: Vote,              //So because the user_id and post_id pairing must be unique, we're protected from the possibility of a suingle user voting on one post multiple times. 
+    as: 'voted_posts',          //This layer of protection is called a foreign key constraint. 
+    foreignKey: 'post_id"'
+});
+
+Vote.belongsTo(User, {          //By also creating one-to-many associations directly between these models, we can perform aggregated SQL functions between models.
+    foreignKey: 'user_id'       //In this case, we'll see a total count of votes for a single post when queried. This would be difficult if we hadn't directly associated the Vote model with the other two.
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models'); //*Refer to Notes.txt to learn more
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models'); //*Refer to Notes.txt to learn more
 //Route that will retrieve all posts in the database. (Use findAll)
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'], //customizing the attributes to include the columns we want from the post table.  
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at'
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ], //customizing the attributes to include the columns we want from the post table.  
         order: [['created_at', 'DESC']], //ensures that the latest articles are shown first to the client.                                                        
         include: [  //creates a JOIN to the User table, which is done by adding the proptery include. Notice that the include property is expressed as an array of objects.
             {       //To define this object, we need a reference to the model and attributes. 
@@ -27,7 +34,13 @@ router.get('/:id', (req, res) => {
         where: { //We used the where property to set the value of the id using req.params.id. Will find posts associated with a particular user_id
             id: req.params.id
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT (*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -56,6 +69,41 @@ router.post('/', (req, res) => {
         user_id: req.body.user_id
     })
         .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// PUT /api/posts/upvote that involves to queries; first, using the Vote model to create a vote, then querying on that post to get an updated vote count.
+router.put('/upvote', (req, res) => { //goes before /:id or Express.js will think the wor "upvote" is a valid parameter for /:id
+    // Custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title
+        },
+        { 
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbPostData);
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -106,4 +154,5 @@ router.delete('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
 module.exports = router; //Make sure to update index.js file in the api folder
