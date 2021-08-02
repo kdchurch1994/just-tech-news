2bdbf4d62a6aeb748df51e6a8c036e18de9c0b08
kdchurch1994 <kdchurch@bsu.edu>
2021-08-02 03:00:47 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create the connection to our database, pass in your MySQL information for username and password (takes this info from the .env file)
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {                                                                                    //See notes on 13.5.6 Deploy to Heroku
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {//When the app is deployed, it will have access to Heroku's process.env.JAWSDB_URL variable and use that value to connect. Otherwise, it will continue using the localhost configuration.
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
+
 
 module.exports = sequelize;
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbUserData => res.json(dbUserData)) // After selecing all of the users from the database it responds with the data as JSON
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
     Comment.create({
         comment_text: req.body.comment_text,
         user_id: req.body.user_id,
@@ -14,12 +23,23 @@ router.get('/', (req, res) => {
         });
 });
 
-router.post('/', (req, res) => {
-
-});
-
 router.delete('/:id', (req, res) => {
-
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id!' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
